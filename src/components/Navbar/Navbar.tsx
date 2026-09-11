eaa0d47298329e1ec9c3542b0e05c9d90b21ae0e
Robin-Majumdar <robinmajumdarofficial@gmail.com>
2026-09-11 16:59:32 +0600

    Add hamburger icon and responsive layout for navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,20 +1,25 @@
+import { HiOutlineMenu } from "react-icons/hi";
 import logo from "../../assets/logo-text.png";
 export default function Navbar() {
 
     return (
         <nav className="sticky top-0 z-50 flex items-center justify-between px-10 py-5 bg-[#F1F5F9]">
 
+            <button className="lg:hidden">
+                <HiOutlineMenu size={24} />
+            </button>
+
             {/* Logo */}
             <div>
                 <img
                     src={logo}
                     alt="Logo"
-                    className="w-32"
+                    className="w-25 lg:w-32"
                 />
             </div>
 
             {/* Navigation Menu */}
-            <ul className="flex items-center gap-8 text-[#475569]">
+            <ul className="hidden lg:flex items-center gap-8 text-[#475569]">
                 <li>
                     <a href="#" className="text-[#DB2777]">Home</a>
                 </li>
@@ -37,7 +42,7 @@ export default function Navbar() {
             </ul>
 
             {/* Buttons */}
-            <div className="flex items-center gap-3">
+            <div className="flex items-center lg:gap-3">
                 <button className="px-5 py-2 text-[#475569]">
                     Sign In
                 </button>
