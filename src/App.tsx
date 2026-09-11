53a46b8b560360c5391b25e6f02bad22522a421c
Robin-Majumdar <robinmajumdarofficial@gmail.com>
2026-09-11 06:40:02 +0600

    Setup a new Dev Stack project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-5xl bg-red-600'>Hello Dev Stack</h1>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn">Secondary</button>
+    </>
+  )
+}
+
+export default App
